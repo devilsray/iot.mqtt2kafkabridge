ad662c00f05d0ab231ec00800e047bc695bb166e
Karsten Kukat <kkukat@devilsray.com>
2021-03-07 13:08:22 +0100

    init

---
+++ src/main/java/com/devilsray/iot/mqtt2kafkabridge/Mqtt2kafkabridgeApplication.java
@@ -0,0 +1,13 @@
+package com.devilsray.iot.mqtt2kafkabridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mqtt2kafkabridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mqtt2kafkabridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devilsray/iot/mqtt2kafkabridge/Mqtt2kafkabridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devilsray.iot.mqtt2kafkabridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mqtt2kafkabridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
